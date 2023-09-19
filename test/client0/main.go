c864d7efa735b1e6de6735f3af2336ef3db89f5d
go75 <2548604505@qq.com>
2023-09-19 15:25:33 +0800

    update

--- test/client0/main.go
+++ test/client0/main.go
@@ -15,53 +15,81 @@ import (
 
 // test0是上传文件的
 func main() {
+	// 连接远程服务端
 	conn, err := net.Dial("tcp", fmt.Sprintf("127.0.0.1:7890"))
 	if err != nil {
 		panic(err)
 	}
 
+	// 当前客户端的名称, 局域网在线的客户端名称唯一的
 	username := "jack"
 
+	// 告诉服务端, 客户端自己叫啥
 	_, err = conn.Write([]byte(username))
 	if err != nil {
 		panic(err)
 	}
 
+	// 接受服务端的回应
 	res := make([]byte, 1)
-
 	_, err = io.ReadFull(conn, res)
 	if err != nil {
 		panic(err)
 	}
 
+	// 如果为0, 证明客户端上线失败, 可能是在线的用户已经有人叫这个名字了, 也有可能是网络问题等等
 	if res[0] == 0 {
 		// 失败
 		panic("连接初始化失败")
 	}
 
+	// 我们要分享的文件信息
+	data, err := json.Marshal(model.ShareFile{
+		ShareUser: username, // 分享人, 当前客户端
+		Path: "/home/v/cv_debug.log", // 分享的文件在我们本机的路径
+		Size: 100, // 文件大小, 我瞎填的, 这个就是一个提示作用, 不影响正常的业务逻辑
+		ShareTime: 10,  // 该文件分享的时间戳, 我瞎填的, 这个就是一个提示作用, 不影响正常的业务逻辑
+		ShareAddress: "127.0.0.1:9999", // 我们分享文件的网络地址, 等待别的客户端连接
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	// 发送服务器, 表明我们分享这个文件
+	_, err = conn.Write(tcpx.Pack(tcpx.NewMessage(common.Set, data)))
+	if err != nil {
+		panic(err)
+	}
+
+	// 启动监听服务, 等待别的客户端来获取当前客户端分享的文件 (p2p传输文件)
 	go func() {
+		// 监听
 		lis, err := net.Listen("tcp", "127.0.0.1:9999")
 		if err != nil {
 			panic(err)
 		}
 		for {
+			// 等待别的客户端连接
 			uploadConn, err := lis.Accept()
 			if err != nil {
 				panic(err)
 			}
+
+			// 连接建立成功后, 读取对方请求的文件路径
 			filePath := make([]byte, 1024)
 			n, err := uploadConn.Read(filePath)
 			if err != nil {
 				panic(err)
 			}
 
-			fmt.Println("要写入文件的路径:", string(filePath))
-
+			// 读取这个文件的内容, 这个步骤要判断一下对方请求的文件在不在当前客户端的文件分享列表中, 为了省事, 我暂时没弄
 			filedata, err := os.ReadFile(string(filePath[:n]))
 			if err != nil {
 				panic(err)
 			}
-			
+
+			// 将读取到文件内容发送给对方客户端, 这里直接一次传输了, 之后可升级多次传输
 			_, err = uploadConn.Write(filedata)
 			if err != nil {
 				panic(err)
@@ -69,33 +97,12 @@ func main() {
 		}
 	}()
 
-	data, err := json.Marshal(model.ShareFile{
-		ShareUser: username,
-		Path: "/home/v/cv_debug.log",
-		Size: 100,
-		ShareTime: 10,
-		ShareAddress: "127.0.0.1:9999",
-	})
-
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println(string(data))
-	_, err = conn.Write(tcpx.Pack(tcpx.NewMessage(common.Set, data)))
-	if err != nil {
-		panic(err)
-	}
-
-	_, err = conn.Write(tcpx.Pack(tcpx.NewMessage(common.Get, nil)))
-	if err != nil {
-		panic(err)
-	}
-
+	// 阻塞, 等待ctrl + c退出
 	exit := make(chan os.Signal, 1)
 	signal.Notify(exit, os.Interrupt)
 	<-exit
 
+	// 取消共享的文件, 或者服务端连接断开就把属于这个用户分享的文件全部删除(这个还没写, 所以现在暂时客户端手动删除)
 	_, err = conn.Write(tcpx.Pack(tcpx.NewMessage(common.Del, data)))
 	if err != nil {
 		panic(err)
--- test/client1/main.go
+++ test/client1/main.go
@@ -15,61 +15,48 @@ import (
 
 // test1是下载文件的
 func main() {
+	// 连接远程服务端
 	conn, err := net.Dial("tcp", fmt.Sprintf("127.0.0.1:7890"))
 	if err != nil {
 		panic(err)
 	}
 
+	// 当前客户端的名称, 局域网在线的客户端名称唯一的
 	username := "bob"
 
+	// 告诉服务端, 客户端自己叫啥
 	_, err = conn.Write([]byte(username))
 	if err != nil {
 		panic(err)
 	}
 
+	// 接受服务端的回应
 	res := make([]byte, 1)
-
 	_, err = io.ReadFull(conn, res)
 	if err != nil {
 		panic(err)
 	}
 
+	// 如果为0, 证明客户端上线失败, 可能是在线的用户已经有人叫这个名字了, 也有可能是网络问题等等
 	if res[0] == 0 {
 		// 失败
 		panic("连接初始化失败")
 	}
 
-	data, err := json.Marshal(model.ShareFile{
-		ShareUser: username,
-		Path: "/",
-		Size: 100,
-		ShareTime: 10,
-		ShareAddress: "127.0.0.1:8888",
-	})
-
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println(string(data))
-	_, err = conn.Write(tcpx.Pack(tcpx.NewMessage(common.Set, data)))
-	if err != nil {
-		panic(err)
-	}
-
+	// 询问服务端当前局域网有哪些分享的文件
 	_, err = conn.Write(tcpx.Pack(tcpx.NewMessage(common.Get, nil)))
 	if err != nil {
 		panic(err)
 	}
 
-
+	// 读取服务端返回的局域网中分享的文件列表
 	fileListData := make([]byte, 4096)
-
 	n, err := conn.Read(fileListData)
 	if err != nil {
 		panic(err)
 	}
 
+	// 文件列表反序列化
 	var fileList = new([]*model.ShareFile)
 	fmt.Println(string(fileListData[3:n]))
 	err = json.Unmarshal(fileListData[3:n], fileList)
@@ -77,34 +64,33 @@ func main() {
 		panic(err)
 	}
 
+	// 拿到client0分享的文件信息
 	fileInfo := (*fileList)[0]
 
+	// 和client0建立连接
 	downloadConn, err := net.Dial("tcp", fileInfo.ShareAddress)
 	if err != nil {
 		panic(err)
 	}
 
+	// 告诉client0我要获取哪个文件
 	_, err = downloadConn.Write([]byte(fileInfo.Path))
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println("写入成功")
-
+	// 读取client0返回的该文件内容
 	fileData := make([]byte, 4096)
 	n, err = downloadConn.Read(fileData)
 	if err != nil {
 		panic(err)
 	}
 
+	// 这里把文件内容打印了, 应该是让客户端选择保存在哪个位置
 	fmt.Printf("recv from %s:%s data:%s\n", fileInfo.ShareUser, fileInfo.ShareAddress, string(fileData))
 
+	// 阻塞, 等待ctrl + c退出
 	exit := make(chan os.Signal, 1)
 	signal.Notify(exit, os.Interrupt)
 	<-exit
-
-	_, err = conn.Write(tcpx.Pack(tcpx.NewMessage(common.Del, data)))
-	if err != nil {
-		panic(err)
-	}
 }
